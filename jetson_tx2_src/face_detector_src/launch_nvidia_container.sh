3602335d0d1d7cc5a0c3df8801c1ad16006cf918
Satya Thiruvallur <sthiruvallur@berkeley.edu>
2020-01-24 00:00:02 -0500

    missed this file

--- jetson_tx2_src/face_detector_src/launch_nvidia_container.sh
+++ jetson_tx2_src/face_detector_src/launch_nvidia_container.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 xhost +
-sudo docker run -it --rm --net=host --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v"$PWD":/HW3  nvcr.io/nvidia/l4t-base:r32.3.1
+sudo docker run -it --rm --net=host --network hw03 --privileged --runtime nvidia  -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v"$PWD":/HW3  face_detect_cntr:latest
